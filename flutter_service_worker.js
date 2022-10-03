88c020a6b8414b7056f94f9f5dcb84354e5ec3fe
atarsdnh <atarsdnh@gmail.com>
2022-10-03 23:52:19 +0800

    fix blank white page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c6f5a5d22874bc12ac15344bb7b6cd2d",
-"/": "c6f5a5d22874bc12ac15344bb7b6cd2d",
+"index.html": "f1b753a806d985683b502eb591d929ac",
+"/": "f1b753a806d985683b502eb591d929ac",
 "main.dart.js": "53ba13560cae7d8c992d1d53dbc9fffe",
 "manifest.json": "2d4ec943864ef99f6190cd7e11b76e40",
 "version.json": "b5e32f30099c7dd71e7b947996a7b616"
